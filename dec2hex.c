5634cf304a8a843af89b0101db252a471210707b
Nyaagoah <146876045+Nyaagoah@users.noreply.github.com>
2024-08-10 12:56:30 -0700

    Update dec2hex.c

--- dec2hex.c
+++ dec2hex.c
@@ -1,6 +1,6 @@
 /**
  * Assignment-1
- * Username:Nyaagoah _______ (fill out your username here)
+ * Username: Nyaagoah _______ (fill out your username here)
  * Class: UCSD CSE30-S224
  */
 #include <stdio.h>
@@ -10,17 +10,14 @@
 
 int main(int argc, char *argv[]) {
 
-
     if (argc != 2) {
-        fprintf(stderr, "%s wrong number of arguments\n", argv[0]);
+        fprintf(stderr, "%s: wrong number of arguments\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-
     unsigned int base10 = (unsigned) atoi(argv[1]);
 
-    // put your code here (use as many lines as you need)
-// Special case for 0
+    // Special case for 0
     if (base10 == 0) {
         printf(POLYNOMIAL_FORMAT, 0, 0);
         return EXIT_SUCCESS;
@@ -32,47 +29,8 @@ int main(int argc, char *argv[]) {
 
     // Convert the number to hexadecimal and store the digits
     while (base10 > 0) {
-        hex_digits[power] = base10 % 16; // Get the least significant digit
-        base10 /= 16; // Move to the next digit
-        power++;
-    }
-
-    /**
- * Assignment-1
- * Username:Nyaagoah _______ (fill out your username here)
- * Class: UCSD CSE30-S224
- */
-#include <stdio.h>
-#include <stdlib.h>
-
-#define POLYNOMIAL_FORMAT "%x * 16^%d\n"
-
-int main(int argc, char *argv[]) {
-
-
-    if (argc != 2) {
-        fprintf(stderr, "%s wrong number of arguments\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-
-    unsigned int base10 = (unsigned) atoi(argv[1]);
-
-    // put your code here (use as many lines as you need)
-// Special case for 0
-    if (base10 == 0) {
-        printf(POLYNOMIAL_FORMAT, 0, 0);
-        return EXIT_SUCCESS;
-    }
-
-    // Array to store hexadecimal digits
-    unsigned int hex_digits[8];
-    int power = 0;
-
-    // Convert the number to hexadecimal and store the digits
-    while (base10 > 0) {
-        hex_digits[power] = base10 % 16; // Get the least significant digit
-        base10 /= 16; // Move to the next digit
+        hex_digits[power] = base10 % 16;
+        base10 /= 16;
         power++;
     }
 
@@ -80,9 +38,6 @@ int main(int argc, char *argv[]) {
     for (int i = power - 1; i >= 0; i--) {
         printf(POLYNOMIAL_FORMAT, hex_digits[i], i);
     }
-
-    return EXIT_SUCCESS;
-}
 
     return EXIT_SUCCESS;
 }
